1648805828650e96be61d755c5489f685b65db23
aeolusheath <313751672@qq.com>
2018-03-18 00:51:30 +0800

    home work 匹配最后一条之前的规则

--- pure-js-demo/ES-6/work/handle.js
+++ pure-js-demo/ES-6/work/handle.js
@@ -39,11 +39,13 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
     //   console.log(item)
     // }
     // step 1  获取代表的单位
-    var reg = new RegExp(/^[a-zA-Z]+ is [IVXLCDM]$/)
+    // var reg = new RegExp(/^\s*[a-zA-Z]+ is [IVXLCDM]\s*$/) //这里需要在最后加上\s mac可以不加，windows默认最后有一个换行符
+    var reg = new RegExp(/^\s*[a-zA-Z]+\s+is\s+[IVXLCDM]\s*$/) //这里需要在最后加上\s mac可以不加，windows默认最后有一个换行符
+    
     var fUnits = []
     if(reg.test(item)) {
-      let fKey = item.split(' is ')[0].trim(),
-        rNum = item.split(' is ')[1].trim()
+      let fKey = item.split(/\s+is\s+/)[0].trim(),
+        rNum = item.split(/\s+is\s+/)[1].trim()//加上replace这个是因为在windows上面最后一个默认是\r换行符 不是空格 在mac上是空格；后续： trim可以去掉所有的空白字符
       // console.log(item, fKey, rNum)
       //todo check the rome number
       // if(!Object.keys( ).includes(rNum)){
@@ -55,25 +57,73 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
 
 
     //step 2 计算 货币的单位
-    var regCom = new RegExp('((glob|prok|pish|tegj)\\s+)+((Silver|Gold|Iron)\\s+){1}(is)(\\s+[1-9]\\d*)(\\s+Credits)')
+    // var regCom = new RegExp('((glob|prok|pish|tegj)\\s+)+((Silver|Gold|Iron)\\s+){1}(is)(\\s+[1-9]\\d*)(\\s+Credits)') //wrong 
+    var regCom = new RegExp(/^\s*((glob|prok|pish|tegj)\s+)+((Silver|Gold|Iron)\s+){1}(is)(\s+[1-9]\d*)(\s+Credits\s*)$/)
+    
     if(regCom.test(item)) {
-      console.log(item, 'here')
+      console.log(item, 'here---------------------------------')
       let sArr = item.split(/\s+is\s+/) //不能直接用is
-      let currenyAndRomensArr = sArr[0].trim().split(' ')
+      let currenyAndRomensArr = sArr[0].trim().split(/\s+/)
       console.log(sArr, currenyAndRomensArr)
       let currency = currenyAndRomensArr.pop(), //IRON
       romensArr = currenyAndRomensArr.slice(0, currenyAndRomensArr.length) //['pish pish']
      
       console.log('当前的货币是: ' +currency, '当前的罗马数字是  ' + romensArr)
       
-      let romensNumber = GetRomenNumber(romensArr, unitsRomen) //将['glob', '']
-
-      let decimals = getDecimalNumber2(romensNumber) 
+      let romensNumber = GetRomenNumber(romensArr, unitsRomen) //将['glob', 'glob'] 转换成 II
+      console.log(romensNumber, '罗马数字')
+      let decimals = getDecimalNumber2(romensNumber)  // 将II 转换成2  将罗马数字转换成十进制数字
       console.log('转换罗马数字之后的结果是： '+  decimals)
-      // currencyMap[currency] = 
+
+      let totalCredentials =  sArr[1].trim().split(/\s+/)[0] //将 3910 credits 的数字部分过滤出来
+
+      currencyMap[currency] = totalCredentials/decimals 
+    }
+
+    //step3 匹配 how much is pish tegj glob glob ?
+    var thirdReg = new RegExp(/^\s*how\s+much\s+is\s+((glob|prok|pish|tegj)\s+)+\s*\?\s*$/)
+    if(thirdReg.test(item)) {
+      let romensStr = item.split(/\s+is\s+/)[1].trim()
+      //去掉最后的问号
+      romensArr = romensStr.replace(/\?|\？/g, '').trim().split(/\s+/)
+      console.log(romensArr, 'romensArrromensArrromensArr')
+      
+      let decimals = getDecimalNumber2(GetRomenNumber(romensArr, unitsRomen))
+      console.log( romensStr.replace(/\?|\？/g, '').trim() + ' is ' + decimals, 'decimalsssss--------------------------------------')
+    }
+
+    //step 4 匹配 how many Credits is glob prok Silver 
+    var fouthReg = new RegExp(/^\s*how\s+many\s+Credits\s+is\s+((glob|prok|pish|tegj)\s+)+(Silver|Gold|Iron){1}\s+\s*\?\s*$/)
+    if(fouthReg.test(item)) {
+      console.log(item)
+      let valueArr = item.split(/\s+is\s+/)[1] // get 'glob prok Silver ?'
     }
   })
   console.log(unitsMap)
+  console.log(currencyMap)
+  /**
+   * 
+   * 
+   * 
+   *  glob is I
+      prok is V
+      pish is X
+      tegj is L
+      glob glob Silver is 34 Credits
+      glob prok Gold is 57800 Credits
+      pish pish Iron is 3910 Credits
+      how much is pish tegj glob glob ?
+      how many Credits is glob prok Silver ?
+      how many Credits is glob prok Silver ?
+      how many Credits is glob prok Gold ?
+      how many Credits is glob prok Iron ?
+      how much wood could a woodchuck chuck if a woodchuck could chuck wood ?
+   * 
+   * 
+   * 
+   */
+
+
     //step2 
 })
 
@@ -85,9 +135,10 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
  * return II
  */
 function GetRomenNumber(arr, obj) {
+  console.log(arr, obj, 'method里面')
   let res = ''
   arr.forEach(item=>{
-    res+=obj[item.trim()]
+    res+=obj[item]
   })
   return res
 /*  
@@ -108,49 +159,16 @@ function GetRomenNumber(arr, obj) {
 
 }
 
-// wrong method
-function getDecimalNumber(romenNumber) {
-  console.log('in here---->>>1111')
-  let arr = romenNumber.split('') //get XXXI
-  // auxiliary method get XX XI
-  let earr = []
-  for(let i=arr.length-1; i >=0; i-=2) {
-    earr.push(arr[i]+ (arr[i-1]||''))
-  }
-  console.log('in here---->>>22222', earr)
-  // return earr
-  var sum = 0
-  earr.forEach((item, index)=> {
-    let first = item.charAt(0)
-    let second = item.charAt(1)
-    if(ROMAN_NUM[first]<ROMAN_NUM[second]) {
-      sum += (ROMAN_NUM[second]- ROMAN_NUM[first])
-      console.log('当前遍历' + index, sum, ROMAN_NUM[second]- ROMAN_NUM[first]) 
-    }
-    else {
-      sum += (ROMAN_NUM[first] + ROMAN_NUM[second]||0)
-      console.log('当前遍历' + index, sum, ROMAN_NUM[first] + ROMAN_NUM[second]||0)
-    }
-  })
-  console.log(sum, 'result')
-  return sum
-}
-// getDecimalNumber('MCMIII')
-// getDecimalNumber('MCMXLIV')
+/**
+ * 
+ * @param {string} romen  "MCX"
+ */
 function getDecimalNumber2 (romen) {
   let totalLength = romen.length
   let sum = 0
   for(let i=0; i< totalLength; i++) {
     let first = ROMAN_NUM[romen.charAt(i)]||0
     let second = ROMAN_NUM[romen.charAt(i+1)]||0
-    // console.log(i, ROMAN_NUM[first], ROMAN_NUM[second], 'process')
-    // if (ROMAN_NUM[first] >= ROMAN_NUM[second]) {
-    //   sum+=ROMAN_NUM[first]
-    // }
-    // else {
-    //   sum += ROMAN_NUM[second] - ROMAN_NUM[first]
-    //   i++
-    // }
     if(first>=second) {
       sum += first
     }else{
