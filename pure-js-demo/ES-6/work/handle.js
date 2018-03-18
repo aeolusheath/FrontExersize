d42db35059400123c82087aece7b46d6309853c6
aeolusheath <313751672@qq.com>
2018-03-18 23:12:07 +0800

    验证roman number是否合法

--- pure-js-demo/ES-6/work/handle.js
+++ pure-js-demo/ES-6/work/handle.js
@@ -1,3 +1,5 @@
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
+
 var path = require('path')
 var fs = require('fs')
 const ROMAN_NUM = {
@@ -72,9 +74,9 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
      
       // console.log('当前的货币是: ' +currency, '当前的罗马数字是  ' + romensArr)
       
-      let romensNumber = GetRomenNumber(romensArr, unitsRomen) //将['glob', 'glob'] 转换成 II
+      let romensNumber = getRomanNum(romensArr, unitsRomen) //将['glob', 'glob'] 转换成 II
       // console.log(romensNumber, '罗马数字')
-      let decimals = getDecimalNumber2(romensNumber)  // 将II 转换成2  将罗马数字转换成十进制数字
+      let decimals = convertRomanToDecimal(romensNumber)  // 将II 转换成2  将罗马数字转换成十进制数字
       // console.log('转换罗马数字之后的结果是： '+  decimals)
 
       let totalCredentials =  sArr[1].trim().split(/\s+/)[0] //将 3910 credits 的数字部分过滤出来
@@ -91,7 +93,7 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
       romensArr = romensStr.replace(/\?|\？/g, '').trim().split(/\s+/)
       // console.log(romensArr, 'romensArrromensArrromensArr')
       
-      let decimals = getDecimalNumber2(GetRomenNumber(romensArr, unitsRomen))
+      let decimals = convertRomanToDecimal(getRomanNum(romensArr, unitsRomen))
       console.log( romensStr.replace(/\?|\？/g, '').trim() + ' is ' + decimals)
     }
 
@@ -104,7 +106,7 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
       // console.log(valueArr, item.split(/\s+is\s+/)[1],' what is this --=========================================')
       let romanStr = valueArr.replace(/\s+(Silver|Gold|Iron){1}\s+\s*\?\s*/, '') //get 'glob prok'
       let romanArr = romanStr.trim().split(/\s+/)
-      let decimals = getDecimalNumber2(GetRomenNumber(romanArr, unitsRomen)) //get 'glob prok' to 4
+      let decimals = convertRomanToDecimal(getRomanNum(romanArr, unitsRomen)) //get 'glob prok' to 4
       let currency = valueArr.match(/\s+(Silver|Gold|Iron)\s+/)[1]
       // console.log('result---', decimals*currencyMap[currency])
       // console.log(valueArr, 'valueArr----------------------------------------------------')
@@ -145,19 +147,17 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
     //step2 
 })
 
-//todo 获取数据 和 获取货币 的数据区分开
+
+
 /**
- * 
- * @param {arry} arry ['glob','glob','Silver']
+ * Get Roman Number 'IV' by Array ['glob','glob']
+ * @param {arry} arry ['glob','glob']
  * @param {object} obj {glob: I, prok: V}
  * return II
  */
-function GetRomenNumber(arr, obj) {
-  // console.log(arr, obj, 'method里面')
+function getRomanNum(arr, obj) {
   let res = ''
-  arr.forEach(item=>{
-    res+=obj[item]
-  })
+  arr.forEach( item => res+=obj[item] )
   return res
 /*  
  *  I 1
@@ -177,11 +177,12 @@ function GetRomenNumber(arr, obj) {
 
 }
 
+
 /**
- * 
- * @param {string} romen  "MCX"
+ * Parse Roman to Decimal : CMXLIV to 994
+ * @param {String} roman 
  */
-function getDecimalNumber2 (romen) {
+function convertRomanToDecimal (romen) {
   let totalLength = romen.length
   let sum = 0
   for(let i=0; i< totalLength; i++) {
@@ -197,10 +198,62 @@ function getDecimalNumber2 (romen) {
   // console.log('方法二', sum)
   return sum
 }
-getDecimalNumber2('MCMIII')
-getDecimalNumber2('MCMXLIV')
+convertRomanToDecimal('MCMIII')
+convertRomanToDecimal('MCMXLIV')
 
-//check is valid Romen Number
-function isValidRomen(romenNumber) {
-  
+
+
+/**
+ * //todo Check is valid roman number
+ * @param {String} romanNum
+ * 
+ * I  1
+ * V  5 -不能减
+ * X  10
+ * L  50 -不能减
+ * C  100
+ * D  500 -不能减
+ * M  1000
+ * 
+ * wiki:https://zh.wikipedia.org/wiki/%E7%BD%97%E9%A9%AC%E6%95%B0%E5%AD%97
+ * stackoverflow: https://stackoverflow.com/questions/267399/how-do-you-match-only-valid-roman-numerals-with-a-regular-expression
+ * 
+ * The symbols "I", "X", "C", and "M" can be repeated three times in succession, but no more. (They may appear four times if the third and fourth are separated by a smaller value, such as XXXIX.) "D", "L", and "V" can never be repeated.
+ * "I" can be subtracted from "V" and "X" only. "X" can be subtracted from "L" and "C" only. "C" can be subtracted from "D" and "M" only. "V", "L", and "D" can never be subtracted.
+ * Only one small-value symbol may be subtracted from any large-value symbol.
+ * A number written in [16]Arabic numerals can be broken into digits. For example, 1903 is composed of 1, 9, 0, and 3. To write the Roman numeral, each of the non-zero digits should be treated separately. Inthe above example, 1,000 = M, 900 = CM, and 3 = III. Therefore, 1903 = MCMIII.
+ *
+ *  I , X , C , M 可以连续重复三次，但是不能更多。  D L V 不能重复
+ *  I 只能被 V 和 X 减， 所以只能出现IV 和 IX ；不能出现: IL ID ID IM 
+ *  X 只能被 L 和 C 减， 所以只能出现XL 和 XC ；不能出现: XD XM
+ *  C 只能被 D 和 M 减， 所以只能出现CD 和 CM
+ *  V L D 不能被减去 不能出现VX VL VC VD VM   LC LD LM  DM 
+ */
+
+// 写单元测试
+function isValidRomanNum(romanNum) {
+  let isValidRomanNum = true
+  let regFirst = /([IXCM])\1{3,}|([DLV])\2{1,}/
+  if(regFirst.test(romanNum)) return false
+  let regSecond = /IL|IC|ID|IM|XD|XM|VX|VL|VC|VD|VM|LC|LC|LM|DM/             //参考IVL  IVX   MVX 1045 是不对的 应该分解为1000+ 40+5 MXLV
+  if(regSecond.test(romanNum)) return false
+
+  //但是要处理特殊情况 IVX roman number 分解成十进制 必须是分解之后的数字 A+B+C+D  A>=B>=C>=D  A B C D 有可能是单个罗马数字 有可能是通过减法运算而来的
+  let pre = 0 
+  for(let i=0; i<romanNum.length; i++) {
+    let first = ROMAN_NUM[romanNum.charAt(i)]||0
+    let second = ROMAN_NUM[romanNum.charAt(i+1)]||0
+    if(pre<first && isValidRomanNum) {
+      isValidRomanNum = false
+    }
+    if(first>=second) {
+      pre = first
+      sum += first
+    }else{
+      pre = second-first
+      sum += (second-first)
+      i++
+    }
+  }
+  return isValidRomanNum
 }
