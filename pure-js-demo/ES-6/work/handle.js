79f2a5f6da457e6e7384593c11536ffe99271bb4
aeolusheath <313751672@qq.com>
2018-03-18 11:23:16 +0800

    home work finish first step[satisfy initial need]

--- pure-js-demo/ES-6/work/handle.js
+++ pure-js-demo/ES-6/work/handle.js
@@ -30,20 +30,21 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
   // console.log(content)
   let rawContent = content
   let lineArr = content.split('\n')
-  console.log(lineArr)
+  // console.log(lineArr)
   var unitsMap = {}, //  { glob: 1, prok: 5, pish: 10,tegi: 50 }
     unitsRomen = {}  //  { glob: I, prok: V, pish: X, tegi: L }
     currencyMap = {} //  { silver: 17, gold: 144500, iron: some number }
   lineArr.forEach(item=> {
-    // if(item.match('/^[a-zA-Z]+ is [IVXLCDM]$/')){
-    //   console.log(item)
-    // }
+    // console.log('当前的字符串是:  ' + item)
+    let isNotMatched = true
+    
     // step 1  获取代表的单位
     // var reg = new RegExp(/^\s*[a-zA-Z]+ is [IVXLCDM]\s*$/) //这里需要在最后加上\s mac可以不加，windows默认最后有一个换行符
     var reg = new RegExp(/^\s*[a-zA-Z]+\s+is\s+[IVXLCDM]\s*$/) //这里需要在最后加上\s mac可以不加，windows默认最后有一个换行符
     
     var fUnits = []
     if(reg.test(item)) {
+      isNotMatched = false
       let fKey = item.split(/\s+is\s+/)[0].trim(),
         rNum = item.split(/\s+is\s+/)[1].trim()//加上replace这个是因为在windows上面最后一个默认是\r换行符 不是空格 在mac上是空格；后续： trim可以去掉所有的空白字符
       // console.log(item, fKey, rNum)
@@ -61,19 +62,20 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
     var regCom = new RegExp(/^\s*((glob|prok|pish|tegj)\s+)+((Silver|Gold|Iron)\s+){1}(is)(\s+[1-9]\d*)(\s+Credits\s*)$/)
     
     if(regCom.test(item)) {
-      console.log(item, 'here---------------------------------')
+      isNotMatched = false
+      // console.log(item, 'here---------------------------------')
       let sArr = item.split(/\s+is\s+/) //不能直接用is
       let currenyAndRomensArr = sArr[0].trim().split(/\s+/)
-      console.log(sArr, currenyAndRomensArr)
+      // console.log(sArr, currenyAndRomensArr)
       let currency = currenyAndRomensArr.pop(), //IRON
       romensArr = currenyAndRomensArr.slice(0, currenyAndRomensArr.length) //['pish pish']
      
-      console.log('当前的货币是: ' +currency, '当前的罗马数字是  ' + romensArr)
+      // console.log('当前的货币是: ' +currency, '当前的罗马数字是  ' + romensArr)
       
       let romensNumber = GetRomenNumber(romensArr, unitsRomen) //将['glob', 'glob'] 转换成 II
-      console.log(romensNumber, '罗马数字')
+      // console.log(romensNumber, '罗马数字')
       let decimals = getDecimalNumber2(romensNumber)  // 将II 转换成2  将罗马数字转换成十进制数字
-      console.log('转换罗马数字之后的结果是： '+  decimals)
+      // console.log('转换罗马数字之后的结果是： '+  decimals)
 
       let totalCredentials =  sArr[1].trim().split(/\s+/)[0] //将 3910 credits 的数字部分过滤出来
 
@@ -83,24 +85,40 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
     //step3 匹配 how much is pish tegj glob glob ?
     var thirdReg = new RegExp(/^\s*how\s+much\s+is\s+((glob|prok|pish|tegj)\s+)+\s*\?\s*$/)
     if(thirdReg.test(item)) {
+      isNotMatched = false      
       let romensStr = item.split(/\s+is\s+/)[1].trim()
       //去掉最后的问号
       romensArr = romensStr.replace(/\?|\？/g, '').trim().split(/\s+/)
-      console.log(romensArr, 'romensArrromensArrromensArr')
+      // console.log(romensArr, 'romensArrromensArrromensArr')
       
       let decimals = getDecimalNumber2(GetRomenNumber(romensArr, unitsRomen))
-      console.log( romensStr.replace(/\?|\？/g, '').trim() + ' is ' + decimals, 'decimalsssss--------------------------------------')
+      console.log( romensStr.replace(/\?|\？/g, '').trim() + ' is ' + decimals)
     }
 
     //step 4 匹配 how many Credits is glob prok Silver 
     var fouthReg = new RegExp(/^\s*how\s+many\s+Credits\s+is\s+((glob|prok|pish|tegj)\s+)+(Silver|Gold|Iron){1}\s+\s*\?\s*$/)
     if(fouthReg.test(item)) {
-      console.log(item)
+      isNotMatched = false      
+      // console.log(item.split(/\s+is\s+/)[1])
       let valueArr = item.split(/\s+is\s+/)[1] // get 'glob prok Silver ?'
+      // console.log(valueArr, item.split(/\s+is\s+/)[1],' what is this --=========================================')
+      let romanStr = valueArr.replace(/\s+(Silver|Gold|Iron){1}\s+\s*\?\s*/, '') //get 'glob prok'
+      let romanArr = romanStr.trim().split(/\s+/)
+      let decimals = getDecimalNumber2(GetRomenNumber(romanArr, unitsRomen)) //get 'glob prok' to 4
+      let currency = valueArr.match(/\s+(Silver|Gold|Iron)\s+/)[1]
+      // console.log('result---', decimals*currencyMap[currency])
+      // console.log(valueArr, 'valueArr----------------------------------------------------')
+      // console.log(item.split(/\s+is\s+/)[1].replace(/\s*\?\s*/, '') + ' is ' + decimals*currencyMap[currency] + ' Credits ')
+      console.log(valueArr.replace(/\s*\?\s*/, '') + ' is ' + decimals*currencyMap[currency] + ' Credits ')
+      
+    }
+
+    if(isNotMatched) {
+      console.log('I have no idea what you are talking about')
     }
   })
-  console.log(unitsMap)
-  console.log(currencyMap)
+  // console.log(unitsMap)
+  // console.log(currencyMap)
   /**
    * 
    * 
@@ -135,7 +153,7 @@ fs.readFile(testInputFile, 'utf8', (err, content)=>{
  * return II
  */
 function GetRomenNumber(arr, obj) {
-  console.log(arr, obj, 'method里面')
+  // console.log(arr, obj, 'method里面')
   let res = ''
   arr.forEach(item=>{
     res+=obj[item]
@@ -176,7 +194,7 @@ function getDecimalNumber2 (romen) {
       i++
     }
   }
-  console.log('方法二', sum)
+  // console.log('方法二', sum)
   return sum
 }
 getDecimalNumber2('MCMIII')
