29890544f76deffe038ddc0cb9c4d6db89e3d10d
aeolusheath <313751672@qq.com>
2018-07-03 21:53:47 +0800

    Meta-programing 元编程->通过实现Iterator接口->设置Symbol.iterator属性为生成器->让对象自身可以用for...of来迭代

--- html/qiwuSTUDY/BetterJS-3/求素数Prime.js
+++ html/qiwuSTUDY/BetterJS-3/求素数Prime.js
@@ -69,5 +69,60 @@
 var primes =[...$limit($getPrimeNumsByGenerator(), 10)]
 
 console.log(primes[primes.length])
+
+
+
+// 升级----素数数列
+const Series = {
+  *sequence(iterator) {
+    let i = 0
+    for(let item of iterator) {
+      yield [i++, item]
+    }
+  },
+  *range(iterator, start, end) {
+    for(let[i, n] of Series.sequence(iterator)) {
+      if(i >= end) break
+      if(i >= start) yield n
+    }
+  }
+}
+
+
+// Meta-Programing 元编程->Symbol Reflect Proxy
+
+const _cache = Symbol('cache')
+
+const PrimeNumbers = {
+  // 通过闭包来缓存这个已经找到的素数数组
+  [_cache]: [2],
   
+  // 构造一个无限循环的生成器，因为生成器需要执行器，我们只需要控制好执行器就好
+  // 但是如果直接使用for(let item of PrimeNumbes)会产生无限循环
+  *[Symbol.iterator]() {
+    const pn = this[_cache]
+    for(let n of pn) {
+      yield n
+    }
+    for(let i = pn[pn.length-1];  i < Infinity; i++) {
+      for(let j =0; j < pn.length; j++) {
+        if(i % pn[j]===0) break
+        if(i< pn[j]**2) {
+          yield i
+          pn.push(i)
+          break
+        }
+      }
+    }
+  }
+}
+
+// 如果不加break，会造成无限循环 然后stackoverflow
+for(let [i, n] of Series.sequence(PrimeNumbers)) {
+  if(i >= 10) break
+  console.log(n)
+}
+
   
+console.log(...Series.range(PrimeNumbers, 1, 10))
+// 这时候可以查看PrimeNumbers，发现Symbol(cache)这个数组会变成10，闭包->缓存数据  
